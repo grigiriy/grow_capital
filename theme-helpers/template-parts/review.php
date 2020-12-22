6774896f462176d4606500aaa30f4cc5a8552b5e
Grigiriy <grigiriy.malyshev@gmail.com>
2020-12-22 08:22:20 +0300

    review structure

---
+++ theme-helpers/template-parts/review.php
@@ -0,0 +1,14 @@
+<div class="col-lg-4">
+  <div class="pd-20 bg-gray cool_review">До этих финансовых откровений считал, что занимаюсь трейдингом. Теперь стало понятно, что играл в лотерею и ходил по самому краю :) Рекомендую всем трейдерам, кто еще не слышал про такие вещи, как волатильность, коэффициент Шарпа, бэктесты и т.д.. Кардинально поменял подход к трейдингу.
+    </div>
+    <div class="facecrd-75 mt-20">
+      <figure>
+        <img src="<?= get_template_directory_uri();?>/assets/img/berdyanskiy.jpg" class="brdr-circ" alt="" />
+        <figcaption>
+          <p class="h5">
+            Бердянский Семен
+          </p>
+        </figcaption>
+      </figure>
+    </div>
+  </div>
