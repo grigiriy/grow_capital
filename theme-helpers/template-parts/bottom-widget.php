b6f8902a3522ebbbd9fecafa007b2edcb71c9dda
Grigiriy <grigiriy.malyshev@gmail.com>
2021-01-25 23:00:33 +0300

    links in bottom widget

--- theme-helpers/template-parts/bottom-widget.php
+++ theme-helpers/template-parts/bottom-widget.php
@@ -2,7 +2,7 @@
   <div class="col-lg-12 infobl mb-30">
     <h2>Что дальше?</h2>
     <p>Поздравляем, теперь вы знаете больше об управлении портфелем. Чтобы использовать полученные знания на
-      практике, вы можете скачать готовый <a href="/" class="fw-6">excel-файл</a>, с помощью которого можно
+      практике, вы можете скачать готовый <a href="https://edu.growcapital.ru/get_excel/" target="_blank" class="fw-6">excel-файл</a>, с помощью которого можно
       вычислить наиболее часто используемые расчеты доходностей.
       Также для более полного понимания темы предлагаем ознакомиться с нижеследующими материалами.</p>
   </div>
@@ -14,7 +14,7 @@
         <img src="<?= get_template_directory_uri(); ?>/assets/img/sistema-investirovaniya.png" alt="">
         <figcaption class="pl-10">
           <p class="h6">
-            <a href="/" class="fc-link">Принципы формирования портфеля</a>
+            <a target="_blank" href="/kak-sostavit-investicionnyj-portfel/" class="fc-link">Принципы формирования портфеля</a>
           </p>
 
         </figcaption>
@@ -23,7 +23,7 @@
     <p class="mb-10 fs-s fw-5">Статья</p>
     <p class="fs-s">Основные этапы и задачи, которые нужно решить, чтобы сформировать оптимальный
       инвестиционный портфель.</p>
-    <a class="btn-small btn-contr hider-9" href="/">Подробнее</a>
+    <a class="btn-small btn-contr hider-9" target="_blank" href="/kak-sostavit-investicionnyj-portfel/">Подробнее</a>
 
   </div>
   <div class="col-lg-4 mb-50 lim-smd lim-md">
@@ -33,7 +33,7 @@
         <img src="<?= get_template_directory_uri(); ?>/assets/img/sistema-investirovaniya.png" alt="">
         <figcaption class="pl-10">
           <p class="h6">
-            <a href="/" class="fc-link">Риски и доходность финансовых инструментов</a>
+            <a target="_blank" href="/kak-vybrat-vo-chto-investirovat/" class="fc-link">Риски и доходность финансовых инструментов</a>
           </p>
 
         </figcaption>
@@ -42,7 +42,7 @@
     <p class="mb-10 fw-5 fs-s">Бесплатный практикум</p>
     <p class="fs-s">Сколько можно заработать или потерять на финансовых рынках. Как оценить риски, связанные с
       инвестированием в определённые инструменты.</p>
-    <a class="btn-small btn-contr hider-9" href="/">Подробнее</a>
+    <a class="btn-small btn-contr hider-9" target="_blank" href="/kak-vybrat-vo-chto-investirovat/">Подробнее</a>
 
   </div>
   <div class="col-lg-4 mb-50 lim-smd lim-md">
@@ -52,7 +52,7 @@
         <img src="<?= get_template_directory_uri(); ?>/assets/img/sistema-investirovaniya.png" alt="">
         <figcaption class="pl-10">
           <p class="h6">
-            <a href="/" class="fc-link">Как заработать, а не потерять? Система инвестирования </a>
+            <a target="_blank" href="/principy-sistematicheskogo-investirovaniya/" class="fc-link">Как заработать, а не потерять? Система инвестирования </a>
           </p>
 
         </figcaption>
@@ -61,7 +61,7 @@
     <p class="mb-10 fw-5 fs-s">Образовательная программа</p>
     <p class="fs-s">Построение персональной системы управления портфелем ценных бумаг на основе
       систематических правил. Принципы и инструменты.</p>
-    <a class="btn-small btn-contr hider-9" href="/">Подробнее</a>
+    <a class="btn-small btn-contr hider-9" target="_blank" href="/principy-sistematicheskogo-investirovaniya/">Подробнее</a>
 
   </div>
 </div>
