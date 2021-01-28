b902470a355d7a661c96aa6b0b927f759c3d5968
Grigiriy <grigiriy.malyshev@gmail.com>
2021-01-28 14:36:00 +0300

    обновил форму

--- theme-helpers/template-parts/forms/services-form.php
+++ theme-helpers/template-parts/forms/services-form.php
@@ -1,22 +1,27 @@
-<form class="form-goriz" id="ltForm1324029" action="https://edu.growcapital.ru/pl/lite/block-public/process-html?id=969693090" method="post" data-open-new-window="0"><input type="hidden" name="formParams[setted_offer_id]"><br>
+<?php
+if(preg_match("/(^|\|)success_form/",implode("|",array_keys($_GET)))){?>
+<h3>Спасибо! Доступ к материалам отправлен вам на почту.</h3>
+<?php } else { ?>
+<form class="form-goriz" id="ltForm5478395" action="https://edu.growcapital.ru/pl/lite/block-public/process-html?id=969693090" method="post" data-open-new-window="0"><input type="hidden" name="formParams[setted_offer_id]"><br>
 	<input type="text" class="wdth-66" maxlength="60" placeholder="Введите ваш эл. адрес" name="formParams[email]" value=""><br>
-	<button type="submit" id="button9673098" class="btn" onclick="if(window['btnprs600f1109e002b']){return false;}window['btnprs600f1109e002b']=true;setTimeout(function(){window['btnprs600f1109e002b']=false},6000);return true;">
+	<button type="submit" id="button5066671" class="btn" onclick="if(window['btnprs60128b218fd4a']){return false;}window['btnprs60128b218fd4a']=true;setTimeout(function(){window['btnprs60128b218fd4a']=false},6000);return true;">
 		Получить</button><br>
-	<input type="hidden" id="329154600f1109d6f13" name="__gc__internal__form__helper" class="__gc__internal__form__helper" value="">
-	<input type="hidden" id="329154600f1109d6f13ref" name="__gc__internal__form__helper_ref" class="__gc__internal__form__helper_ref" value="">
-	<input type="hidden" name="requestTime" value="1611600137">
-	<input type="hidden" name="requestSimpleSign" value="e8c7142137b63fc6a4aadeaf2584f9be">
+	<input type="hidden" id="32915460128b2180f82" name="__gc__internal__form__helper" class="__gc__internal__form__helper" value="">
+	<input type="hidden" id="32915460128b2180f82ref" name="__gc__internal__form__helper_ref" class="__gc__internal__form__helper_ref" value="">
+	<input type="hidden" name="requestTime" value="1611828001">
+	<input type="hidden" name="requestSimpleSign" value="db62be44ba9705ebc7efa7d6dd3e010c">
 	<input type="hidden" name="isHtmlWidget" value="1" />
 </form>
 <span id="gccounterImgContainer"></span>
 <script>
 	window.addEventListener('load', function() {
-		let loc = document.getElementById("329154600f1109d6f13");
+		let loc = document.getElementById("32915460128b2180f82");
 		loc.value = window.location.href;
-		let ref = document.getElementById("329154600f1109d6f13ref");
+		let ref = document.getElementById("32915460128b2180f82ref");
 		ref.value = document.referrer;
 		let statUrl = "https://edu.growcapital.ru/stat/counter?ref=" + encodeURIComponent(document.referrer) +
 			"&loc=" + encodeURIComponent(document.location.href);
 		document.getElementById('gccounterImgContainer').innerHTML = "<img width=1 height=1 style='display:none' id='gccounterImg' src='" + statUrl + "'/>";
 	});
 </script>
+<?php } ?>
