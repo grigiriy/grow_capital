328a3f2fb0d13e23ff1248c6c8569e0322640326
Grigiriy <grigiriy.malyshev@gmail.com>
2021-01-28 09:38:22 +0300

    гк-чекбокс в боковой виджет вебинара

--- theme-helpers/custom-fields/widgets/webinar.php
+++ theme-helpers/custom-fields/widgets/webinar.php
@@ -9,6 +9,7 @@ Block::make(__('Webinar Form'))
       ->set_html('<h2 style="color:blue; padding: 20px;">Форма c вебинаром</h2>'),
     Field::make( 'select', 'webinar_id', 'Вебинар:' )
       ->add_options( get_webinars_arr() ),
+    Field::make('checkbox', 'is_gc', 'ссылка на гк'),
     ])
   ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
 
@@ -23,7 +24,7 @@ Block::make(__('Webinar Form'))
         <img src="<?= get_template_directory_uri(); ?>/assets/img/chart-4.png" alt="">
         <figcaption class="pl-10">
           <p class="h5">
-            <a href="<?= carbon_get_post_meta($course_id, 'link'); ?>" class="fc-link fc-light-9"><?= carbon_get_post_meta($course_id, 'headline'); ?></a>
+            <a href="<?= $fields['is_gc'] ? carbon_get_post_meta($course_id, 'link') : get_the_permalink($course_id); ?>" class="fc-link fc-light-9"><?= carbon_get_post_meta($course_id, 'headline'); ?></a>
           </p>
           <div class="svg-black fs-s">
             <div class="icon-listed">
@@ -35,7 +36,7 @@ Block::make(__('Webinar Form'))
     </div>
     <p class="mb-10 fw-5"><?= carbon_get_post_meta($course_id, 'subtitle'); ?></p>
     <div class="fs-s"><?= carbon_get_post_meta($course_id, 'lead'); ?></div>
-    <a href="<?= carbon_get_post_meta($course_id, 'link'); ?>" class="btn-small-pt">Посмотреть
+    <a href="<?= $fields['is_gc'] ? carbon_get_post_meta($course_id, 'link') : get_the_permalink($course_id); ?>" class="btn-small-pt">Посмотреть
       <span class="icon-angle-right"> </span>
     </a>
 
