f459a4af90e05213d25af88201f9fadcbb7ba66a
Grigiriy <grigiriy.malyshev@gmail.com>
2021-01-28 09:44:09 +0300

    фикс верстки "Пустого" макета

--- page-clear.php
+++ page-clear.php
@@ -10,14 +10,8 @@ get_header(); ?>
 
 <section class="header mb-60">
   <div class="container-w">
-    <div class="row mb-20 pt-40">
-      <div class="col-xl-offset-3 col-xl-9 fs-xs fw-3">
-        <?php get_template_part('theme-helpers/template-parts/breadcrumbs'); ?>
-      </div>
-    </div>
     <div class="row mb-30">
-        <?php the_content(); ?>
-      </div>
+      <?php the_content(); ?>
     </div>
   </div>
 </section>
