a05bf3ca06544fea3b1d346b0b108657fc709b12
Grigiriy <grigiriy.malyshev@gmail.com>
2021-02-17 22:43:37 +0300

    textpage template

--- page.php
+++ page.php
@@ -1,12 +1,14 @@
 <?php
-
 get_header();
 
+while (have_posts()) : the_post(); ?>
 
-while (have_posts()) : the_post(); 
-
-the_content();
-
-endwhile;
-
+<section class="mb-50">
+  <div class="container-w">
+    <div class="row mb-30 pt-60 mt-30">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</section>
+<?php endwhile;
 get_footer(); ?>
