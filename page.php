3c3a81cd74e111f596215942742972f7d3279a09
Grigiriy <grigiriy.malyshev@gmail.com>
2021-02-19 21:32:52 +0300

    lil fix

--- page.php
+++ page.php
@@ -3,9 +3,10 @@ get_header();
 
 while (have_posts()) : the_post(); ?>
 
-<section class="mb-50">
+<section class="mb-50 mt-70-9">
   <div class="container-w">
-    <div class="mb-30 pt-60 mt-30">
+    <div class="mb-30">
+      <h1 class="fs-xl"><?php the_title(); ?></h1>
       <?php the_content(); ?>
     </div>
   </div>
